924f730f8419d4d31529f35948332e2e34ba6b1a
kbub27 <kalebmerriman27@gmail.com>
2019-03-27 21:37:54 -0700

    ajax query function

--- main.js
+++ main.js
@@ -15,3 +15,25 @@ $('#searchbtn').on('click', function() {
     var endYear = $('#endYear').val();
     response = querySearch(searchTerm, numberOfRecords, startYear, endYear);
 });
+
+function querySearch(searchTerm, numRecords, startYear, endYear) {
+
+    var link;
+    var queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + searchTerm + '&begin_date=' + startYear + '&end_date=' + endYear + '&api-key=M8GKFxeUmc32YlzOgyRkpjvZRUddW6wB'
+    var headline;
+    var authors;
+
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).then(function (response) {
+        for (let i = 0; i < numRecords; index++) {
+            console.log(response);
+            link = response.response.docs[i].web_url;
+            headline = response.response.docs[i].headline.main;
+            authors = response.response.docs[i].byline.original;
+            console.log(response.response.docs[i].headline.main);
+            console.log(response.response.docs[i].byline.original);
+        }
+    });
+};
